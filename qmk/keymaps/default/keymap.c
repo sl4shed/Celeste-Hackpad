79241f127400ebfb3a3f3dfb6eaab79ac02669de
Andrew <sl4shed@proton.me>
2026-04-04 13:10:13 +0300

    fix qmk firmware

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -11,18 +11,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ z │ x │ c │ v │   │   │ < │ v │ > │
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
-    /*
+    
     [0] = LAYOUT(
-        KC_A, KC_S, KC_D, KC_F, _______, KC_UP, _______,
-        KC_Z, KC_X, KC_C, KC_V, KC_LEFT, KC_DOWN, KC_RGHT
-    )};
-    */
+        KC_NO,   KC_UP,   KC_NO,   KC_NO,
+        KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,
+        KC_A,    KC_S,    KC_D,    KC_F,
+        KC_Z,    KC_X,    KC_C,    KC_V
+    )
+};
 
-    [0] = LAYOUT(KC_A, KC_S, KC_D, KC_F, KC_UP, KC_Z, KC_X, KC_C, KC_V, KC_LEFT, KC_DOWN, KC_RGHT)};
-
-#ifdef OLED_ENABLE
-
-#    include <stdio.h>
-#    include <oled.c>
-
-#endif // OLED_ENABLE
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom(0, 255, 120);
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_MOOD);
+}
